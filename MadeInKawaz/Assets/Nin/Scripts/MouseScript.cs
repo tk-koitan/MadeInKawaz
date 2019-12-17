6c171e1757460a202d3f49458f75f626360a9436
Tsurumiya <visitor96@gmail.com>
2019-12-17 21:18:05 +0900

    ネズミのスピード2倍化

--- MadeInKawaz/Assets/Nin/Scripts/MouseScript.cs
+++ MadeInKawaz/Assets/Nin/Scripts/MouseScript.cs
@@ -25,7 +25,7 @@ public class MouseScript : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0) && !isFinished)
         {
-            transform.position += new Vector3(-1, 0, 0);
+            transform.position += new Vector3(-2, 0, 0);
             spriteVersion++;
             if (spriteVersion > 3)
                 spriteVersion = 0;
