79e56b5ba5b1463ba43ad0c424e729e8f3b9944f
Tsurumiya <visitor96@gmail.com>
2019-12-17 20:38:43 +0900

    競走ゲーム

---
+++ MadeInKawaz/Assets/CatScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatScript : MonoBehaviour
+{
+    private float t = 0;
+    private Vector3 startPosition;
+    private Vector3 target;
+    [SerializeField]
+    private float timeToReachTarget = 5f;
+    void Start()
+    {
+        startPosition = transform.position;
+        target = new Vector3(-14, 0, 0);
+    }
+    void Update()
+    {
+        t += Time.deltaTime / timeToReachTarget;
+        transform.position = Vector3.Lerp(startPosition, target, t);
+    }
+}
---
+++ MadeInKawaz/Assets/Nin/Scripts/MouseScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScript : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer youSprite;
+    [SerializeField]
+    private Sprite[] sprites;
+    private int spriteVersion = 0;
+
+    private bool isFinished = false;
+    private bool isWin = false;
+    [SerializeField]
+    private Transform cat;
+    //private AudioSource audioSource;
+    
+    void Start()
+    {
+        //audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !isFinished)
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            spriteVersion++;
+            if (spriteVersion > 3)
+                spriteVersion = 0;
+            youSprite.sprite = sprites[spriteVersion];
+            //audioSource.Play();
+        }
+        if (!isFinished)
+        {
+            if (transform.position.x < -6 || cat.position.x < -4.5f)
+            {
+                transform.parent = cat;
+                isFinished = true;
+                if (transform.position.x < -6)
+                    isWin = true;
+            }
+        }
+
+        if (isFinished)
+        {
+            if (isWin)
+            {
+                GameManager.Clear();
+                isWin = false;
+            }
+        }
+    }
+}
