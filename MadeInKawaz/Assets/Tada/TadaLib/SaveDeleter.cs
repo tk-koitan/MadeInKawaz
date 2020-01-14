4cce1b451b19e63b9cc24e219cba471571cfe9ad
taisei <tatata0389@gmail.com>
2020-01-14 18:33:50 +0900

    セーブデータをボタンで削除できるようにした

---
+++ MadeInKawaz/Assets/Tada/TadaLib/SaveDeleter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TadaLib.Save;
+
+namespace Save
+{
+    public class SaveDeleter : MonoBehaviour
+    {
+        public void DeleteData()
+        {
+            SaveManager.Instance.DeleteAllData();
+        }
+    }
+}
