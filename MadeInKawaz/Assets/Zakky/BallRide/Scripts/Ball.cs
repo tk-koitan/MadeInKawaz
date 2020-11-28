79bca2ab74435504580a8194d4f4eba23127942f
Zakky3 <zakky0317@eis.hokudai.ac.jp>
2020-11-28 18:14:32 +0900

    BallRide追加

---
+++ MadeInKawaz/Assets/Zakky/BallRide/Scripts/Ball.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody2D mRigidbody2D;
+    float mBallRadius;
+
+    // マウスの一フレーム前の座標
+    Vector3 oldPos;
+
+    void Awake()
+    {
+        transform.position = new Vector3(Random.Range(-1f, 1f), -1f, 0f);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        mRigidbody2D = GetComponent<Rigidbody2D>();
+        mBallRadius = GetComponent<CircleCollider2D>().radius;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 touchScreenPosition = Input.mousePosition;
+
+        // 10.0fに深い意味は無い。画面に表示したいので適当な値を入れてカメラから離そうとしているだけ.
+        touchScreenPosition.z = 10.0f;
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(touchScreenPosition);
+
+        float vel = SignOrZero(Input.GetAxis("Horizontal"));
+        if (Input.GetMouseButtonDown(0))
+        {
+            oldPos = mousePos;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            //transform.Translate(new Vector3(mousePos.x - oldPos.x, 0f, 0f));
+            vel = SignOrZero(mousePos.x - oldPos.x);
+            //画面外に出ないように
+            //transform.position = new Vector3(Mathf.Clamp(transform.position.x, -width, width), Mathf.Clamp(transform.position.y, -height, height));
+            oldPos = mousePos;
+        }
+
+        mRigidbody2D.velocity += new Vector2(100f *  vel * Time.deltaTime, 0f);
+        transform.rotation = Quaternion.Euler(0f, 0f, Mathf.Rad2Deg * -transform.position.x / mBallRadius);
+
+        
+    }
+
+    float SignOrZero(float a)
+    {
+        if (a == 0) return 0;
+        else return Mathf.Sign(a);
+    }
+}
---
+++ MadeInKawaz/Assets/Zakky/BallRide/Scripts/Cat.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    [SerializeField]
+    Ball mBall;
+
+    float mBallRadius;
+    float mCatRadius;
+
+    float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ボールと猫の半径求める
+        mBallRadius = mBall.GetComponent<CircleCollider2D>().radius * mBall.transform.localScale.x;
+        mCatRadius = GetComponent<CircleCollider2D>().radius * transform.localScale.x;
+
+        transform.position = new Vector3(
+            Random.Range(0, 2) == 0 ? Random.Range(-0.2f, -0.1f) : Random.Range(0.1f, 0.2f)
+            + mBall.transform.position.x,
+            mBall.transform.position.y + 4f,
+            0f
+            );
+
+        time = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ボールに猫が乗ってるとき
+        if (transform.position.y > mBall.transform.position.y)
+        {
+            //ボールから猫へのベクトル
+            Vector3 vec = transform.position - mBall.transform.position;
+            //猫が浮いてるときボールと猫の距離常に一定
+            if (vec.magnitude > mBallRadius + mCatRadius)
+            {
+                transform.position = vec.normalized * (mBallRadius + mCatRadius)
+                + mBall.transform.position;
+            }
+            
+            //猫をボールに合わせて回転
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, Mathf.Rad2Deg * Mathf.Atan2(vec.y, vec.x) - 90f));
+        }
+
+        //とりあえずクリア書いた
+        time += Time.deltaTime;
+        if (time > 3.8f && transform.position.y > -3.5f) GameManager.Clear();
+    }
+}
