94f235a5e6bf7ac16f00f584263d1af7c815c2be
Malen <kyoshuma7@gmail.com>
2024-05-25 21:36:49 +0900

    まーれん シーン追加

---
+++ MadeInKawaz/Assets/Malen/Scripts/GameManager_Malen.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager_Malen : MonoBehaviour
+{
+    [SerializeField] GameObject[] people;
+    [SerializeField] Sprite correctSprite;
+    [SerializeField] ParticleSystem particle;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip correctSE;
+    [SerializeField] AudioClip incorrectSE;
+    private bool flag;
+    private GameObject correctPerson;
+
+    private void Start()
+    {
+        flag = false;
+        correctPerson = people[Random.Range(0, people.Length)];
+        correctPerson.GetComponent<Image>().sprite = correctSprite;
+    }
+
+    public void OnClick()
+    {
+        if (flag == false)
+        {
+            GameObject selectedObject = EventSystem.current.currentSelectedGameObject;
+            foreach (GameObject person in people)
+            {
+                if(person != correctPerson){
+                    person.GetComponent<Button>().interactable = false;
+                }
+            }
+            if(selectedObject == correctPerson){
+                particle.transform.position = correctPerson.transform.position;
+                particle.Play();
+                audioSource.PlayOneShot(correctSE);
+                GameManager.Clear();
+            }else{
+                audioSource.PlayOneShot(incorrectSE);
+            }
+        }
+        flag = true;
+    }
+}
--- MadeInKawaz/Assets/meiteiFox/Scripts/ShootScript.cs
+++ MadeInKawaz/Assets/meiteiFox/Scripts/ShootScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.Experimental.PlayerLoop;
+//using UnityEngine.Experimental.PlayerLoop;
 
 public class ShootScript : MonoBehaviour
 {
