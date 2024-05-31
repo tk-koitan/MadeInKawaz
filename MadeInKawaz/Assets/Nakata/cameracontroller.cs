dde0f983c51ed6a6280d41631a78d4430a85e9bf
SHIN <106070536+FRESH-SHIN@users.noreply.github.com>
2024-05-31 11:24:16 +0900

    add: nakataのtuna cut

---
+++ MadeInKawaz/Assets/Nakata/cameracontroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameracontroller : MonoBehaviour
+{
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(timekeeper.time);
+        if (timekeeper.time >= 1.75f)
+        {
+            Debug.Log("Camera Moved");
+            transform.position = new Vector3(30, 0, -10);
+        }
+    }
+}
---
+++ MadeInKawaz/Assets/Nakata/masterknife.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class masterknife : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(demo());
+        IEnumerator demo()
+        {
+            yield return new WaitForSeconds(0.5f);
+            cut();
+            yield return new WaitForSeconds(0.25f);
+            cut();
+            yield return new WaitForSeconds(0.25f);
+            cut();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timekeeper.time<=1.5f)
+        {
+            transform.Translate(7.998f*Time.deltaTime, 1.5f*Time.deltaTime, 0);
+        }
+    }
+    void cut()
+    {
+        transform.Translate(0, -2, 0);
+        StartCoroutine(Up());
+        IEnumerator Up()
+        {
+            yield return new WaitForSeconds(0.1f);
+            transform.Translate(0, 2, 0);
+        }
+    }
+}
