7c0a84b14516716388ff430c2aa98b7e8a37342c
SHIN <106070536+FRESH-SHIN@users.noreply.github.com>
2024-06-01 21:01:41 +0900

    add: fuzeのコーヒー牛乳

---
+++ MadeInKawaz/Assets/Fuze/Script/Kamipakku.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kamipakku : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("x軸の回転角度")]
+    private float rotateX = 0;
+    
+    [SerializeField]
+    [Tooltip("y軸の回転角度")]
+    private float rotateY = 0;
+
+    [SerializeField]
+    [Tooltip("z軸の回転角度")]
+    private float rotateZ = 50;
+    private static bool buttonFlag;
+
+    public static bool getButtonFlag()
+    {
+        return buttonFlag;
+    }
+
+    public void Start()
+    {
+        Debug.Log("1" + buttonFlag);
+        buttonFlag = false;
+    }
+    public void Update()
+    {
+        if(buttonFlag == true)
+        {
+            
+        }
+        else if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("3" + buttonFlag);
+            buttonFlag = true;
+        }
+        else
+        {        
+            // X,Y,Z軸に対してそれぞれ、指定した角度ずつ回転させている。
+            // deltaTimeをかけることで、フレームごとではなく、1秒ごとに回転するようにしている。
+            gameObject.transform.Rotate(new Vector3(rotateX, rotateY, rotateZ) * Time.deltaTime);
+        }
+    }
+
+    
+}
