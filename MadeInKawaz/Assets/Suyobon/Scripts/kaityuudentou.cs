45a4692446a48be7596f8202eb391cd6aed88072
SHIN <106070536+FRESH-SHIN@users.noreply.github.com>
2024-06-07 11:23:37 +0900

    add: suyobon game

---
+++ MadeInKawaz/Assets/Suyobon/Scripts/kaityuudentou.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kaityuudentou : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+    private Rigidbody2D rb;
+    [SerializeField]
+    private GameObject obake;
+    [SerializeField]
+    private GameObject flashLight;
+    [SerializeField]
+    private Sprite obake_mabusii;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 flashPos = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 5f));
+        flashPos.z = 5f;
+        rb.MovePosition(flashPos);
+
+        if(Input.GetMouseButton(0))
+        {
+            GameObject clickObj = null;
+            
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if(Physics.Raycast(ray, out hit))
+            {
+                clickObj = hit.collider.gameObject;
+
+                if(clickObj = obake)
+                {
+                    obake.GetComponent<SpriteRenderer>().sprite = obake_mabusii;
+                    obake.GetComponent<Animator>().SetBool("clear", true);
+                    GetComponent<AudioSource>().Play();
+                    GameManager.Clear();
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ MadeInKawaz/Assets/Suyobon/Scripts/obake.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obake : MonoBehaviour
+{
+    private Transform tra;
+    [SerializeField]
+    private Transform rangeA;
+    [SerializeField]
+    private Transform rangeB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tra = this.transform;
+        float x = Random.Range(rangeA.position.x, rangeB.position.x);
+        float y = Random.Range(rangeA.position.y, rangeB.position.y);
+        tra.position = new Vector3(x, y, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
