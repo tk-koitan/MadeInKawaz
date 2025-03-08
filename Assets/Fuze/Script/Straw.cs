7b4433ddee0b61a01acc44312e2e3c417f9adb57
NI-yy <iy.nozomy@gmail.com>
2025-03-08 14:32:01 +0900

    fuzeくんのスクリプト修正(not depending FPS

--- Assets/Fuze/Script/Straw.cs
+++ Assets/Fuze/Script/Straw.cs
@@ -8,12 +8,21 @@ public class Straw : MonoBehaviour
 {
     [SerializeField]
     float speed;
+    [SerializeField] private float decreasRate = 0.1f;
+
     void Update()
     {
         if(Kamipakku.getButtonFlag() == true)
         {
             transform.Translate(0,this.speed * Time.deltaTime,0);
-            this.speed *= 0.98f;
+            //this.speed *= 0.98f;
+
+
+            // revise not to depend on frame rate. (by y_y 2025_03_01
+            var beta = 1f - Mathf.Pow(1 - decreasRate, 60f * Time.deltaTime);
+            this.speed = Mathf.Lerp(this.speed, 0f, beta);
+
+            Debug.Log(speed);
         }
         else if(Input.GetMouseButtonDown(0))
         {
